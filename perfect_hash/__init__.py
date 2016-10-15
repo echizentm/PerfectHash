e9a722329a3fd6b36c2a78c43e6e3898fbeffc76
echizen_tm <echizentm@gmail.com>
2016-10-15 22:48:31 +0900

    add check(), size()

--- perfect_hash/__init__.py
+++ perfect_hash/__init__.py
@@ -6,12 +6,15 @@ class PerfectHash():
         self.k = 0
         self.blocks = []
 
+    # 自然数の集合 elements と
+    # elements がとりうる値よりも大きい素数 p を渡して
+    # ハッシュを作ります
     def make(self, elements, p):
         n = len(elements)
         self.k = self._get_k(elements, p, n, 'col1')
 
         sub_elements = []
-        for q in elements:
+        for i in range(n):
             sub_elements.append([])
 
         for q in elements:
@@ -23,6 +26,8 @@ class PerfectHash():
 
         return self
 
+    # 自然数 q が make() で渡した elements の中に
+    # 存在するかどうかの情報を得ます
     def membership(self, q, p):
         block = self.blocks[(self.k * q) % p % len(self.blocks)]
         n = len(block['blocks'])
@@ -32,6 +37,39 @@ class PerfectHash():
             return False
         else:
             return True
+
+    # ハッシュの精度を計算します
+    # 1.0 が返ってこない場合, なんらかの不具合があります
+    def check(self, elements, p):
+        valid_count = 0
+        total_count = 0
+        for q in range(1, p):
+            in_elements = q in elements
+            in_blocks = self.membership(q, p)
+            if in_elements and in_blocks:
+                valid_count += 1
+            elif (not in_elements) and (not in_blocks):
+                valid_count += 1
+            total_count += 1
+        return valid_count / total_count
+
+    # ハッシュに必要なブロック数を取得します
+    #
+    # 普通の自然数の配列の場合, ブロック数が配列長 n で
+    # membership() の時間計算量が O(n) になります
+    #
+    # 完全ハッシュの場合はブロック数が n + o(n) で
+    # membership() の時間計算量が O(1) になります
+    #
+    # ブロック数に関する詳細な情報は README の論文を参照してください
+    def size(self):
+        count = 1
+        for block in self.blocks:
+            count += 1
+            n = len(block['blocks'])
+            if n > 1:
+                count += (n + 2)
+        return count
 
     def _make_block(self, elements, p):
         n = len(elements) ** 2
@@ -53,7 +91,7 @@ class PerfectHash():
                 blocks[(k * q) % p % n] += 1
             if (mode == 'col1') and self._check_col1(blocks, n):
                 return k
-            if (mode == 'col2') and self._check_col2(blocks, n):
+            if (mode == 'col2') and self._check_col2(blocks):
                 return k
         return p
 
@@ -63,7 +101,7 @@ class PerfectHash():
             count += block ** 2
         return True if count < (3 * n) else False
 
-    def _check_col2(self, blocks, n):
+    def _check_col2(self, blocks):
         for block in blocks:
             if block > 1:
                 return False
--- sample.py
+++ sample.py
@@ -1,12 +1,34 @@
 # coding: utf-8
+import random
+from argparse import ArgumentParser
 from perfect_hash import PerfectHash
 
 
-elements = [2, 4, 5, 15, 18, 30]
-p = 31
+parser = ArgumentParser(
+    description='sample code of PerfectHash'
+)
+parser.add_argument(
+    '-p', '--prime',
+    type=int, default=10009,
+    help='universe set size (prime number)',
+)
+parser.add_argument(
+    '-s', '--size',
+    type=int, default=300,
+    help='subset size to sample',
+)
+parser.add_argument(
+    '-t', '--trials',
+    type=int, default=10,
+    help='number of trials',
+)
+args = parser.parse_args()
 
 ph = PerfectHash()
-ph.make(elements, p)
-for q in range(1, p):
-    if (ph.membership(q, p)):
-        print("{0} is member.".format(q))
+for i in range(args.trials):
+    elements = random.sample(range(1, args.prime), args.size)
+    ph.make(elements, args.prime)
+    if ph.check(elements, args.prime) == 1.0:
+        print("Success. block size: {0}".format(ph.size()))
+    else:
+        print("Failure.")
